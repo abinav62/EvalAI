f0316b738bab7af1d48acc50902dc903bcc7216b
abinav62 <abichari62@gmail.com>
2020-01-06 21:57:20 -0500

    Update urls.py

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     url(r"^user/disable$", views.disable_user, name="disable_user"),
     url(r"^user/get_auth_token$", views.get_auth_token, name="get_auth_token"),
-    url(r"^user/resend_confirmation_email$", views.resend_confirmation_email, name="resend_email_confirmation"),
 ]
